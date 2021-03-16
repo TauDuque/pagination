aaeabbf0745257adbff8679ff4d140acdc45456d
Tau <hectauduque@gmail.com>
2021-03-16 17:55:50 -0300

    fim

--- src/App.js
+++ src/App.js
@@ -3,14 +3,37 @@ import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
   const { loading, data } = useFetch();
-  console.log(data);
   const [page, setPage] = useState(0);
   const [followers, setFollowers] = useState([]);
 
   useEffect(() => {
     if (loading) return;
     setFollowers(data[page]);
-  }, [loading]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
 
   return (
     <main>
@@ -23,6 +46,27 @@ function App() {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              Voltar
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              Pŕoxima
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
--- src/Follower.js
+++ src/Follower.js
@@ -6,7 +6,7 @@ const Follower = ({ avatar_url, html_url, login }) => {
       <img src={avatar_url} alt={login} />
       <h4>{login}</h4>
       <a href={html_url} className="btn">
-        Visualizar Perfil
+        Visualizar&nbsp;Perfil
       </a>
     </article>
   );
